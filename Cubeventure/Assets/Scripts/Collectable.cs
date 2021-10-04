24bc46afc0f4f42ec2afb5904e3a7592d1881f7e
Pucho21 <puchovskylukas21@gmail.com>
2021-10-04 14:10:30 +0200

    Initial Commit

---
+++ Cubeventure/Assets/Scripts/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public enum collectableType
+    {
+        star,
+        coin,
+        redGem,
+        greenGem,
+        blueGem,
+        purpleGem,
+    }
+    public collectableType druh;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Collected " + druh);
+            Destroy(gameObject);
+        }
+    }
+}
