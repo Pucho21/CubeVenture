5eb87b10c19007404026cc7b1728fd32ca692e94
MihalikJ <jojjom10@gmail.com>
2021-10-06 09:14:23 +0200

    kostra main menu + cinemachine kamerky

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public enum collectableType
+    {
+        star,
+        coin,
+        redGem,
+        greenGem,
+        blueGem,
+        purpleGem,
+        cyanGem,
+        yellowGem
+    }
+    public collectableType druh;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Collected " + druh);
+            Destroy(gameObject);
+        }
+    }
+}
